834d782dfcf696378d39c6e9fca6c0ffa3cabcae
kennedy <sferreira.kennedy@gmail.com>
2024-08-09 15:26:25 +0100

    utils create

--- internal/api/api.go
+++ internal/api/api.go
@@ -179,9 +179,68 @@ func (h apiHandler) handleCreateRoom(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write(data)
 }
 
+func (h apiHandler) readRoom(
+	w http.ResponseWriter,
+	r *http.Request,
+) (room pgstore.Room, rawRoomID string, roomID uuid.UUID, ok bool) {
+	rawRoomID = chi.URLParam(r, "room_id")
+	roomID, err := uuid.Parse(rawRoomID)
+	if err != nil {
+		http.Error(w, "invalid room id", http.StatusBadRequest)
+		return pgstore.Room{}, "", uuid.UUID{}, false
+	}
+
+	room, err = h.q.GetRoom(r.Context(), roomID)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			http.Error(w, "room not found", http.StatusBadRequest)
+			return pgstore.Room{}, "", uuid.UUID{}, false
+		}
+
+		slog.Error("failed to get room", "error", err)
+		http.Error(w, "something went wrong", http.StatusInternalServerError)
+		return pgstore.Room{}, "", uuid.UUID{}, false
+	}
+
+	return room, rawRoomID, roomID, true
+}
+
+func sendJSON(w http.ResponseWriter, rawData any) {
+	data, _ := json.Marshal(rawData)
+	w.Header().Set("Content-Type", "application/json")
+	_, _ = w.Write(data)
+}
+
 func (h apiHandler) handleGetRooms(w http.ResponseWriter, r *http.Request) {}
 
-func (h apiHandler) handleGetRoomMessage(w http.ResponseWriter, r *http.Request) {}
+func (h apiHandler) handleGetRoomMessage(w http.ResponseWriter, r *http.Request) {
+	_, _, _, ok := h.readRoom(w, r)
+	if !ok {
+		return
+	}
+
+	rawMessageID := chi.URLParam(r, "message_id")
+	messageID, err := uuid.Parse(rawMessageID)
+	if err != nil {
+		http.Error(w, "invalid message id", http.StatusBadRequest)
+		return
+	}
+
+	messages, err := h.q.GetMessage(r.Context(), messageID)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			http.Error(w, "message not found", http.StatusBadRequest)
+			return
+		}
+
+		slog.Error("failed to get message", "error", err)
+		http.Error(w, "something went wrong", http.StatusInternalServerError)
+		return
+	}
+
+	sendJSON(w, messages)
+}
+
 func (h apiHandler) handleCreateRoomMessages(w http.ResponseWriter, r *http.Request) {
 
 	rawRoomID := chi.URLParam(r, "room_id")
